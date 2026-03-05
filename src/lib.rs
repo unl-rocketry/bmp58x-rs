1185a48be53663e250af2153bc42fc9a18f80c7e
ElliottSuh <elliottsuh09@gmail.com>
2026-03-04 21:26:27 -0600

    Started working on the lib, there is cannot find trait `I2c` error on line 8 and another error on line 14

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+const BMP5_CHIP_ID: u8 = 0x50;
+
+enum Address {
+    Primary = 0x46,
+    Secondary = 0x47
+}
+
+pub struct BMP5<I: I2c> {
+    address: Address,
+    i2c: I
+}
+
+
+impl<I, E> BMP5<I>{
+    pub fn new(
+        mut i2c: I,
+
+    ) -> Self {
+        
+    }
+}
+
+pub struct Measurement{
+    temperature: f32,
+    pressure: Option<f32>,
 }
