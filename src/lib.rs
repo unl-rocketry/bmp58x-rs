0c3eea79ad1c1158dfd12e2a0473f9d1b8725fdb
ElliottSuh <elliottsuh09@gmail.com>
2026-03-12 10:46:18 -0500

    Done some work on the BMP5XX part, though it is incomplete, also got the error. For most of the code, is like copy and paste with some change?

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use embedded_hal_async::delay::DelayNs;
+
 const BMP5_CHIP_ID: u8 = 0x50;
 
 enum Address {
@@ -8,17 +10,30 @@ enum Address {
 pub struct BMP5<I: I2c> {
     address: Address,
     i2c: I
+
+}
+pub enum Error<E> {
+    I2c(E),
+    WrongChip(u8),
+    Fatal,
+    Command,
+    Configuration,
 }
 
 
-impl<I, E> BMP5<I>{
-    pub fn new(
-        mut i2c: I,
-
-    ) -> Self {
+impl<I, E> BMP5<I>
+where
+    I: I2c<Error = E>,
+{
+    pub async fn try_new<D: DelayNs>(
+    mut i2c: I,
+    address: Address,
+    mut delay: D,
+    config: &Configuration,
+    ) -> Result<Self, Error<E>>
         
-    }
 }
+
 
 pub struct Measurement{
     temperature: f32,
